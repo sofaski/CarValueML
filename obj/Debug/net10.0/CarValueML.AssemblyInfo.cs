d020bedd8af33a0bbd781df8290b38576282cd1c
sofaski <sofiia.postelniak@nure.ua>
2026-03-23 14:30:51 +0200

    Initial commit ASP.NET Core MVC ML project

--- obj/Debug/net10.0/CarValueML.AssemblyInfo.cs
+++ obj/Debug/net10.0/CarValueML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarValueML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490b24c06c678459dc7812120b84f73a6f766933")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarValueML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarValueML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
